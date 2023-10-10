dd287b336f7be62fbf8d6af01b976953f9359aa0
Yevhen <ko4etov.evgeniy@gmail.com>
2023-10-10 18:28:58 +0300

    Create todo-redux-app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import logo from "./logo.svg"
+import { Counter } from "./features/counter/Counter"
+import "./App.css"
+
+function App() {
+  return (
+    <div>HEllo</div>
+  )
+}
+
+export default App
