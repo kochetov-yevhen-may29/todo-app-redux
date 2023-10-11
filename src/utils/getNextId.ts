1ae95c175c31e6cf5c14e77a9f5dd67c76f39149
Yevhen <ko4etov.evgeniy@gmail.com>
2023-10-11 23:02:09 +0300

    Prepare for deployment

--- src/utils/getNextId.ts
+++ src/utils/getNextId.ts
@@ -1,4 +1,4 @@
-import { Todo } from "../redux/slices/todoSlice";
+import { Todo } from "../types/Todo";
 
 export const getNextId = (todos: Todo[]) => {
   const maxId = Math.max(...todos.map(todo => todo.id), 0);
